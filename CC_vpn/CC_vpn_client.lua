36fdd88cbdb579dbe68d9c43ef194be561b2bf5a
FilippChudakov <168969409+FilippChudakov@users.noreply.github.com>
2025-06-25 21:31:49 +0300

    Update CC_vpn_client.lua
    VPN Client 1.5

--- CC_vpn/CC_vpn_client.lua
+++ CC_vpn/CC_vpn_client.lua
@@ -2,20 +2,15 @@ local original_rednet_send = rednet.send
 local original_rednet_receive = rednet.receive
 
 function rednet.send(receiver, message, protocol)
-    local fake_receiver = 11
+    local fake_receiver = 0
 
-    original_rednet_send(fake_receiver, "VPN:redirect", "VPN")
-    original_rednet_send(fake_receiver, message, "VPN")
-    original_rednet_send(fake_receiver, receiver, "VPN")
-    original_rednet_send(fake_receiver, protocol, "VPN")
+    original_rednet_send(fake_receiver, {"VPN:redirect", receiver, message, protocol}, "VPN")
 end
 
 function rednet.receive(protocol_filter, timeout)
     local id, msg, protocol = original_rednet_receive(nil, timeout)
     if protocol == "VPN" then
-        savemsg = msg
-        local id, msg, protocol = original_rednet_receive(nil, 1)
-        return tonumber(msg), savemsg, protocol
+        return tonumber(msg[1]), msg[2], msg[3]
     else
         return id, msg, protocol
     end
