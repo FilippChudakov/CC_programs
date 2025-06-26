07b8ed7e86018631e9adfe910ee147a683320a23
FilippChudakov <168969409+FilippChudakov@users.noreply.github.com>
2025-06-26 17:15:40 +0300

    Update Network.lua
    Update Network.lua to 2.5

--- RangerBank/RangerBankClient/APIS/Network.lua
+++ RangerBank/RangerBankClient/APIS/Network.lua
@@ -4,7 +4,19 @@ Network.ID = 0
 Network.Error = nil
 
 function Network.version()
-    return "RangerBank 2.1"
+    return "RangerBank 2.5"
+end
+
+function Network.changelog(version)
+    if version == "2.0" then
+        return "Reworked graphics and add protocol"
+    elseif version == "2.1" then
+        return "Fixed some bugs"
+    elseif version == "2.5" then
+        return "Reworked rednet use"
+    else
+        return "not a version"
+    end
 end
 
 function Network.open()
@@ -16,56 +28,54 @@ function Network.close()
 end
 
 function Network.send(ID, SendId, Messages, Protocol)
-    rednet.send(ID, SendId, Protocol)
+    local message = {}
     for _, Message in pairs(Messages) do
-        rednet.send(ID, Message, Protocol)
+        table.insert(message, Message)
     end
+    local data = Network.BankTable(SendId, message)
+    rednet.send(ID, data, Protocol)
 end
 
 function Network.receive(Protocol, TimeOut)
-    local id, send_id, protocol = rednet.receive(Protocol, TimeOut)
+    local id, message, protocol = rednet.receive(Protocol, TimeOut)
 
     if Network.ID == id then
-        if send_id == "RangerBank: 1" then
-            while true do
-                local id, message = rednet.receive(Protocol, 1)
-
-                if id ~= nil and message ~= nil then
-                    if Network.ID == id then
-                        return "Complete!", message
-                    end
-                else
-                    break
-                end
-            end
-        elseif send_id == "RangerBank: 2" then
-            while true do
-                local id, message = rednet.receive(Protocol, 1)
-
-                if id ~= nil and message ~= nil then
-                    if Network.ID == id then
-                        return "Error", message
-                    end
-                else
-                    break
-                end
-            end
-        elseif send_id == "RangerBank: 3" then
-            while true do
-                local id, message = rednet.receive(Protocol, 1)
-
-                if id ~= nil and message ~= nil then
-                    if Network.ID == id then
-                        return "Money", message
-                    end
-                else
-                    break
-                end
-            end
+        if message[1] == "RangerBank: 1" then
+            return "Complete!", message[2]
+        elseif message[1] == "RangerBank: 2" then
+            return "Error", message[2]
+        elseif message[1] == "RangerBank: 3" then
+            return "Money", message[2]
         else
             return "Error", "Wrong send ID"
         end
     end
+end
+
+function Network.BankTable(SendId, data)
+    local new_data = {SendId}
+    if SendId == "RangerBank:get_money" or SendId == "RangerBank:login" or SendId == "RangerBank:register" or SendId ==  "RangerBank:delete_account" or SendId == "RangerBank:get_log" then
+        new_data["password"] = data[1]
+        new_data["account"] = data[2]
+    elseif SendId == "RangerBank:minus_money" or SendId == "RangerBank:add_money" then
+        new_data["account"] = data[1]
+        new_data["summ"] = data[2]
+        new_data["secret_pass"] = data[3]
+    elseif SendId == "RangerBank:change_password" then
+        new_data["password"] = data[1]
+        new_data["new_pass"] = data[2]
+        new_data["account"] = data[3]
+    elseif SendId == "RangerBank:transfer_money" then
+        new_data["password"] = data[1]
+        new_data["summ"] = data[2]
+        new_data["receiver"] = data[3]
+        new_data["account"] = data[4]
+    elseif SendId == "RangerBank:first_login_log" then
+        new_data["account"] = data[1]
+    elseif SendId == "RangerBank:OFF" then
+        new_data["secret_pass"] = data[1]
+    end
+    return new_data
 end
 
 return Network
