6cf2670dc44c8d3fe948fadef0f34bab6a9e13b2
FilippChudakov <168969409+FilippChudakov@users.noreply.github.com>
2026-05-04 00:23:00 +0300

    Enhance VPN ID input and handling logic
    Updated VPN ID handling to allow user input for fake ID, with options to disable VPN or use a random ID. Improved function definitions for getting computer ID based on user input.

--- CC_vpn/RednetVPN/vpn.lua
+++ CC_vpn/RednetVPN/vpn.lua
@@ -1,12 +1,42 @@
---print("Enter fake ID:")
---fake_id = tonumber(read())
-fake_id = math.random(10, rednet.MAX_ID_CHANNELS - 1)
-print("Using fake ID: " .. fake_id)
+print("Enter fake ID or\nX - disable\n-1 - use random id:")
+fake_id = read()
 
-function os.getComputerID()
-    return fake_id
+if fake_id == "-1" then
+    fake_id = math.random(10, rednet.MAX_ID_CHANNELS - 1)
 end
 
-function os.computerID()
-    return fake_id
+if fake_id == "X" or fake_id == "x" then
+    if _G.id then
+        local real_id = _G.id
+        function os.getComputerID()
+            return real_id
+        end
+
+        function os.computerID()
+            return real_id
+        end
+
+        _G.id = nil
+
+        print("vpn disabled")
+    else
+        print("vpn disabled")
+    end
+elseif tonumber(fake_id) then
+    fake_id = tonumber(fake_id)
+
+    print("Using fake ID: " .. fake_id)
+
+    if not _G.id then
+        _G.id = os.getComputerID()
+    end
+
+    function os.getComputerID()
+        return fake_id
+    end
+
+    function os.computerID()
+        return fake_id
+    end
+
 end
